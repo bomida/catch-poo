6196c274fd319b5d4bae3689708d0ab3f2e1ad2d
BomiDo <bm.do.bomhada@gmail.com>
2022-03-28 14:11:58 +0900

    정크 파일 정리

--- src/main.js
+++ src/main.js
@@ -2,61 +2,33 @@
 
 const SEA_LIFE_COUNT = 10;
 const POO_COUNT = 10;
+const ITEM_SIZE = 90;
 const GAME_DURATION_SEC = 10;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
-const gameBtn = document.querySelector('.game__button');
 const gameTitle = document.querySelector('.game__title');
+const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
 const popup = document.querySelector('.popup');
-const popupMessage = document.querySelector('.popup__message');
 const popupRefresh = document.querySelector('.popup__refresh');
-
-const carrotSound = new Audio('./sound/carrot_pull.mp3');
-const bugSound = new Audio('./sound/bug_pull.mp3');
-const bgSound = new Audio('./sound/bg.mp3');
-const alertSound = new Audio('./sound/alert.wav');
-const winSound = new Audio('./sound/game_win.mp3');
+const popupMessage = document.querySelector('.popup__message');
 
 let started = false;
 let timer = undefined;
 let score = 0;
 
-const icons = [
-  {
-    className: 'poo',
-    count: SEA_LIFE_COUNT,
-    imgPath: './img/poo.png'
-  },
-  {
-    className: 'sealife',
-    count: SEA_LIFE_COUNT,
-    imgPath: './img/fish.png'
-  },
-  {
-    className: 'sealife',
-    count: SEA_LIFE_COUNT,
-    imgPath: './img/shark.png'
-  },
-  {
-    className: 'sealife',
-    count: SEA_LIFE_COUNT,
-    imgPath: './img/seahorse.png'
-  }
-]
-
 field.addEventListener('click', onFieldClick);
 
 popupRefresh.addEventListener('click', () => {
-  gameStart();
   hidePopup();
-});
+  gameStart();
+})
 
 gameBtn.addEventListener('click', () => {
-  if(started) {
+  if (started) {
     gameStop();
   } else {
     gameStart();
@@ -69,38 +41,28 @@ function gameStart() {
   gameInit();
   showStopBtn();
   startGameTimer();
-  playSound(bgSound);
 }
 
 function gameStop() {
   started = false;
-  hideGameBtn();
   stopGameTimer();
-  stopSound(bgSound);
-  playSound(alertSound);
-  showPopupWithText('REPLAY?🧐');
+  hideGameBtn();
+  showPopupWithText('REPLAY?');
 }
 
 function gameFinish(win) {
-  started = false;
-  hideGameBtn();
-  if(win) {
-    playSound(winSound);
-  } else {
-    stopSound(bugSound);
-  }
-  stopSound(bgSound);
   stopGameTimer();
-  showPopupWithText(win? 'YOU WIN:)':'YOU LOSE:(');
+  hideGameBtn();
+  showPopupWithText(win ? 'YOU WIN:)' : 'YOU LOSE:(');
 }
 
 function startGameTimer() {
   let remainingTimeSec = GAME_DURATION_SEC;
   updateTimerText(remainingTimeSec);
   timer = setInterval(() => {
-    if(remainingTimeSec <= 0) {
+    if (remainingTimeSec <= 0) {
       clearInterval(timer);
-      finishGame(POO_COUNT === score);
+      gameFinish();
       return;
     }
     updateTimerText(--remainingTimeSec);
@@ -117,16 +79,24 @@ function updateTimerText(time) {
   gameTimer.innerText = `${minute}:${second}`;
 }
 
-function showStopBtn() {
-  const icon = document.querySelector('.fas');
-  icon.classList.add('fa-stop');
-  icon.classList.remove('fa-play');
-  gameBtn.classList.remove('hide');
+function updateScoreBoard() {
+  gameScore.innerText = POO_COUNT - score;
 }
 
 function showPopupWithText(text) {
-  popupMessage.innerText = text;
   popup.classList.remove('hide');
+  popupMessage.innerText = text;
+}
+
+function showStopBtn() {
+  const icon = document.querySelector('.fas');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-start');
+  gameBtn.classList.remove('hide');
+}
+
+function hideTitle() {
+  gameTitle.classList.add('hide');
 }
 
 function hideGameBtn() {
@@ -137,39 +107,29 @@ function hidePopup() {
   popup.classList.add('hide');
 }
 
-function hideTitle() {
-  gameTitle.classList.add('hide');
-}
-
 function onFieldClick(event) {
-  if(!started) {
-    return;
+  if (!started) {
+    return
   }
   const target = event.target;
-  if(target.matches('.poo')) {
+  if (target.matches('.poo')) {
     target.remove('poo');
     score++;
-    playSound(carrotSound);
     updateScoreBoard();
-    if(score === POO_COUNT) {
+    if (POO_COUNT === score) {
       gameFinish(true);
     }
-  } else if(target.matches('.sealife')){
+  } else if (target.matches('.sealife')) {
     gameFinish(false);
-    playSound(bugSound);
   }
 }
 
-function updateScoreBoard() {
-  gameScore.innerText = POO_COUNT - score;
-}
-
-function addItem(className, count, imgPath) {
+function addItme(className, count, imgPath) {
   const x1 = 0;
   const y1 = 0;
-  const x2 = fieldRect.width - 90;
-  const y2 = fieldRect.height - 90;
-  for(let i = 0; i < count; i++) {
+  const x2 = fieldRect.width - ITEM_SIZE;
+  const y2 = fieldRect.height - ITEM_SIZE;
+  for (let i = 0; i < count; i++) {
     const item = document.createElement('img');
     item.setAttribute('class', className);
     item.setAttribute('src', imgPath);
@@ -186,22 +146,14 @@ function gameInit() {
   score = 0;
   field.innerHTML = '';
   gameScore.innerText = POO_COUNT;
-  addItem('poo', POO_COUNT, './img/poo.png');
-  addItem('sealife', SEA_LIFE_COUNT, './img/fish.png');
-  addItem('sealife', SEA_LIFE_COUNT, './img/shark.png');
-  addItem('sealife', SEA_LIFE_COUNT, './img/jellyfish.png');
-  addItem('sealife', SEA_LIFE_COUNT, './img/seahorse.png');
+  addItme('poo', POO_COUNT, './img/poo.png');
+  addItme('sealife', SEA_LIFE_COUNT, './img/fish.png');
+  addItme('sealife', SEA_LIFE_COUNT, './img/jellyfish.png');
+  addItme('sealife', SEA_LIFE_COUNT, './img/turtle.png');
+  addItme('sealife', SEA_LIFE_COUNT, './img/seahorse.png');
+  addItme('sealife', SEA_LIFE_COUNT, './img/shark.png');
 }
 
 function randomNum(min, max) {
   return Math.random() * (max - min) + min;
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
-}
-
-function stopSound(sound) {
-  sound.pause();
 }
